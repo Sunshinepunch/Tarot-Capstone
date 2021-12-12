16c51c04752fbff114386742c1f17f902194ce1f
Sunshinepunch <78851729+Sunshinepunch@users.noreply.github.com>
2021-12-12 15:57:13 -0800

    updating directory, adding webpack for header

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import TarotControl from "./TarotControl";
+
+function App(){
+  return ( 
+    <React.Fragment>
+      <Header />
+      <TarotControl />
+    </React.Fragment>
+  );
+}
+
+export default App;
