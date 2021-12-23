8880c77ca321a56d862189804e545924198fa0f3
Sunshinepunch <78851729+Sunshinepunch@users.noreply.github.com>
2021-12-22 19:13:08 -0800

    card shuffle working, animation working, fling not working, cursor not attaching correctly

---
+++ src/images.js
@@ -0,0 +1,158 @@
+const images = [
+  {
+    "id": 0,
+    "name": "The Fool",
+    "image": "../images/Fool.jpg",
+    "imageback": "../images/CardBack.png",
+    "desc": "The Fool strides bravely forward, unaware of the dangers that surround him. Yet is this blissful ignorance the key to something profound?"
+  },
+  {
+    "id": 1,
+    "name": "The Magician",
+    "image": "../images/Magician.jpg",
+    "imageback": "../images/CardBack.png",
+    "desc": "The Magician possesses the tools of change, earned from personal trial and tribulation. He will see his vision manifested from the fruits of his own labors."
+  },
+  {
+    "id": 2,
+    "name": "The High Priestess",
+    "image": "../images/Priestess.jpg",
+    "imageback": "../images/CardBack.png",
+    "desc": "The Priestess is the divine conduit of the unconscious and the unseen - she reaches deeply into the well of the soul to draw forth wisdom. Trust that which lies beneath."
+  },
+  {
+    "id": 3,
+    "name": "The Empress",
+    "image": "../images/Empress.jpg",
+    "imageback": "../images/CardBack.png",
+    "desc": "The Empress is mother to all - nurturing, feminine, abundant. She brings comfort with her mere presence."
+  },
+  {
+    "id": 4,
+    "name": "The Emperor",
+    "image": "../images/Emperor.jpg",
+    "imageback": "../images/CardBack.png",
+    "desc": "The rock upon which the Empire stands is the Emperor himself - absolute authority and unyielding resolution. Steadfast yet heavy is the head that wears the crown."
+  },
+  {
+    "id": 5,
+    "name": "The Hierophant",
+    "image": "../images/Hierophant.jpg",
+    "imageback": "../images/CardBack.png",
+    "desc": "Established master of tradition, the Hierophant knows that lofty ideals are built on tried fundamentals. Adhere to the creed and it shall see you through."
+  },
+  {
+    "id": 6,
+    "name": "The Lovers",
+    "image": "../images/Lovers.png",
+    "imageback": "../images/CardBack.png",
+    "desc": "The Lovers embody raw honesty and open communication. Nothing can be hidden, and more importantly there is nothing to hide. To know and be known is love."
+  },
+  {
+    "id": 7,
+    "name": "The Chariot",
+    "image": "../images/Chariot.jpg",
+    "imageback": "../images/CardBack.png",
+    "desc": "Onward, the Chariot draws you - this is no time for passivity. Be bold brave one, but not too bold, lest your lifeblood runs a'cold."
+  },
+  {
+    "id": 8,
+    "name": "Strength",
+    "image": "../images/Strength.jpg",
+    "imageback": "../images/CardBack.png",
+    "desc": "Strength is not the exertion of a moment, but the endurance of a lifetime. Divine the source of your power and become strong within and without. Remember - strength is controlled, not bestial waving."
+  },
+  {
+    "id": 9,
+    "name": "The Hermit",
+    "image": "../images/Hermit.jpg",
+    "imageback": "../images/CardBack.png",
+    "desc": "The Hermit knows that a raindrop cannot know itself within a waterfall. Take space and retreat, let introspection and solitude guide your fortune. "
+  },
+  {
+    "id": 10,
+    "name": "The Wheel of Fortune",
+    "image": "../images/Wheel.jpg",
+    "imageback": "../images/CardBack.png",
+    "desc": "The Wheel is always turning, the seas of Fortune are ever-churning. No matter how good or ill the current tides, the past ebbs as the future arrives."
+  },
+  {
+    "id": 11,
+    "name": "Justice",
+    "image": "../images/Justice.jpg",
+    "imageback": "../images/CardBack.png",
+    "desc": "Justice knows that no matter how far you run, there you are. She knows because she is waiting there for you."
+  },
+  {
+    "id": 12,
+    "name": "The Hanged Man",
+    "image": "../images/Hanged Man.jpg",
+    "imageback": "../images/CardBack.png",
+    "desc": "The Hanged Man is forced to pause. He takes the chance to see things from a new perspective. Make use of even the worst things and grow."
+  },
+  {
+    "id": 13,
+    "name": "Death",
+    "image": "../images/Death.jpg",
+    "imageback": "../images/CardBack.png",
+    "desc": "New growth cannot begin without destruction of the old. Death is not to be feared, but do not rush to seek it. A time of change is upon you."
+  },
+  {
+    "id": 14,
+    "name": "Temperance",
+    "image": "../images/Temperance.jpg",
+    "imageback": "../images/CardBack.png",
+    "desc": "In all things moderation. Temperance tells us to find the balance in our indulgences, and keep patience in our work."
+  },
+  {
+    "id": 15,
+    "name": "The Devil",
+    "image": "../images/Devil.png",
+    "imageback": "../images/CardBack.png",
+    "desc": "All light casts a shadow - your inner light is no different. The Devil reminds you of the negative forces that bind you from your true potential. Cast aside your chains and be truly free."
+  },
+  {
+    "id": 16,
+    "name": "The Tower",
+    "image": "../images/Tower.jpg",
+    "imageback": "../images/CardBack.png",
+    "desc": "The Tower warns of an unexpected change - usually to something thought too sturdy to be shaken. To be forewarned is to be forearmed. Check your premises."
+  },
+  {
+    "id": 17,
+    "name": "The Star",
+    "image": "../images/Star.jpg",
+    "imageback": "../images/CardBack.png",
+    "desc": "The Star is a beacon, to guide and to give hope. Seek to elevate yourself, and rediscover the joy in your aspirations."
+  },
+  {
+    "id": 18,
+    "name": "The Moon",
+    "image": "../images/Moon.jpg",
+    "imageback": "../images/CardBack.png",
+    "desc": "The Moon deals in illusion and uncertainty - nothing is as it seems. Seek out information and attempt to connect with your subconscious mind."
+  },
+  {
+    "id": 19,
+    "name": "The Sun",
+    "image": "../images/Sun.jpg",
+    "imageback": "../images/CardBack.png",
+    "desc": "The Sun is radiance and abundance personified - warmth begets warmth, light begets light, and success begets success. Let your energy shine forth with a jolly incandescence."
+  },
+  {
+    "id": 20,
+    "name": "Judgement",
+    "image": "../images/Judgement.jpg",
+    "imageback": "../images/CardBack.png",
+    "desc": "At the end of the path lies Judgement - a spiritual awakening is signified. You are close to the end. Complete your journey and achieve your most spiritual aim."
+  },
+  {
+    "id": 22,
+    "name": "The World",
+    "image": "../images/World.jpg",
+    "imageback": "../images/CardBack.png",
+    "desc": "No one can be so apart as to be apart from The World. Fulfillment and completion are yours. Reflect and rest, you who have gone so far."
+  }
+]
+
+export default images;
